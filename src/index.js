7567056394823cbf0bca2933b4b49a33d6966c30
Abhishek Rastogi <abhirastogi3800@gmail.com>
2026-07-09 12:41:38 +0530

    Adding packages for tracking user activity

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/index.css";
 import App from "@/App";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +20,8 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </QueryClientProvider>
   </React.StrictMode>,
 );
